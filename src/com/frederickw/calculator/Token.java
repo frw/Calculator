361fa298884cbc5296a4c6c57dc47395d5206a5d
Frederick <frederick_widjaja@hotmail.com>
2012-06-27 08:02:44 +0700

    Updated Tokens

--- src/com/frederickw/calculator/Token.java
+++ src/com/frederickw/calculator/Token.java
@@ -2,10 +2,10 @@ package com.frederickw.calculator;
 
 public interface Token {
 
-    public enum Type {
-        NUMBER, CONSTANT, VARIABLE, OPERATION, FUNCTION, PARENTHESIS
-    }
+	public enum Type {
+		NUMBER, CONSTANT, OPERATION, FUNCTION, PARENTHESIS
+	}
 
-    public Type getType();
+	public Type getType();
 
 }
---
+++ src/com/frederickw/calculator/token/Function.java
@@ -0,0 +1,5 @@
+package com.frederickw.calculator.token;
+
+public final class Function {
+
+}
